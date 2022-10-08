6f655157f156850aa713b3a2efe321b4e6dad341
Quang Rau <lethanhquang910@gmail.com>
2022-10-08 12:07:30 +0800

    feat: update data

--- routes.ts
+++ routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "./deps.ts";
 import { getActivity, getActivities } from "./controllers/activity.ts";
-import { getAnnoucements } from "./controllers/annoucement.ts";
+import { getAnnouncements } from "./controllers/announcement.ts";
 import { getDocuments } from "./controllers/document.ts";
 
 const router = new Router();
@@ -9,7 +9,7 @@ const version = "v1";
 router
   .get(`/${version}/activities`, getActivities)
   .get(`/${version}/activities/:id`, getActivity)
-  .get(`/${version}/annoucements`, getAnnoucements)
+  .get(`/${version}/announcements`, getAnnouncements)
   .get(`/${version}/documents`, getDocuments);
 
 export default router;
